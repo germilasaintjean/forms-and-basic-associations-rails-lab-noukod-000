3c32310f97db39c7ff18be3329b018d633e78575
Saint-Jean Germila <germila@noukod.com>
2019-02-05 18:55:09 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,27 +3,33 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def genre_name=(name)
-    self.genre = Genre.find_by_name(name)
+  def genre_name= name
+    #since the song has no genre, we have to create it
+    self.genre = Genre.find_or_create_by name: name
   end
 
   def genre_name
-    self.genre.name
+    #if the song has a genre return the genre's name else return nil
+    genre ? genre.name : nil
   end
 
-  def artist_name(name)
-    self.artist = Artist.find_by_name(name)
+  def artist_name= name
+    self.artist = Artist.find_or_create_by name: name
   end
 
   def artist_name
-    self.artist.name
+    artist ? artist.name : nil
   end
 
-  def note_contents(content)
-    self.note = Note.find_by_content(content)
+  def note_contents= contents
+    contents.each do |c|
+      self.notes << Note.find_or_create_by(content: c) unless c.empty?
+    end
   end
 
   def note_contents
-    self.note.contents
-  end
+    notes.map do |n|
+      n.content
+    end
+end
 end

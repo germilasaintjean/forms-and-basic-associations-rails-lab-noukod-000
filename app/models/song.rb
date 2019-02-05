b55f94dee369d73c06e2aa0bbf17a98bfdb67609
Saint-Jean Germila <germila@noukod.com>
2019-02-05 18:47:47 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,20 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_by_name(name)
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def artist_name(name)
+    self.artist = Artist.find_by_name(name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+end
